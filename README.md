1152b5bc99f48799d352d354e6bebf5df21c36c6
Jay <jnelson@LTCF99.local>
2024-08-04 14:02:39 -0400

    update avd client name and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # az-gov-avd-session-reset-portal
-A simple flask app that removes avd users disconnected sessions
+A simple flask app that removes avd users disconnected sessions.<br>
+Scoped to be used in azure gov cloud. <br>
+Currently in development, not recomended for use.
+
+
+### TODO:
+* scope to be used in avd remote app
+* re-work auth to use app. registration 
+* error handling
+* re-work session removal to do graceful logoff first 
+* create chrome policy to app.
+* logging
+* create generic install
--- session_reset.py
+++ session_reset.py
@@ -9,7 +9,7 @@ AZURE_DESKTOPVIRTUALIZATION_MGMT_CLIENT_API_VERSION = '2019-09-24-preview'
 
 
 # TODO add error handling
-def create_avd_client():
+def create_avd_client_session():
     credential = DefaultAzureCredential(authority=AzureAuthorityHosts.AZURE_GOVERNMENT)
     return DesktopVirtualizationMgmtClient(
         credential=credential,
@@ -26,7 +26,7 @@ def get_avd_session_hosts(client, rg_name, host_pool_name):
 
 def list_host_pool_user_session_data():
     api_data = {}
-    avd_client = create_avd_client()
+    avd_client = create_avd_client_session()
     list_user_sessions = avd_client.user_sessions.list_by_host_pool(AZURE_RESOURCE_GROUP_NAME, AZURE_HOST_POOL_NAME)
     api_data['data'] = list_user_sessions
     if api_data['data']:
@@ -59,7 +59,7 @@ def end_user_avd_session(user_upn):
         'session_host_name'].split('/')
     session_host_name = parse_session_host_and_user_session_id[1]
     user_session_id = parse_session_host_and_user_session_id[2]
-    avd_client = create_avd_client()
+    avd_client = create_avd_client_session()
     return avd_client.user_sessions.delete(AZURE_RESOURCE_GROUP_NAME, AZURE_HOST_POOL_NAME, session_host_name,
                                            user_session_id, force=True)
 
