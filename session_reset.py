399bf7adcba31345d3ffbba25aad7cbd25e18043
Jay <jnelson@LTCF99.local>
2024-08-09 18:41:30 -0400

    refactored to find user across all host pools and to dynamicly find the rg for the host pool

--- session_reset.py
+++ session_reset.py
@@ -1,11 +1,12 @@
-
+import inspect
 from azure.identity import DefaultAzureCredential, AzureAuthorityHosts
 from azure.mgmt.desktopvirtualization import DesktopVirtualizationMgmtClient
 
 AZURE_SUBSCRIPTION_ID = ''
 AZURE_RESOURCE_GROUP_NAME = ''
-AZURE_HOST_POOL_NAME = ''
 AZURE_DESKTOPVIRTUALIZATION_MGMT_CLIENT_API_VERSION = '2019-09-24-preview'
+
+api_data = {}
 
 
 # TODO add error handling
@@ -16,54 +17,77 @@ def create_avd_client_session():
         subscription_id=AZURE_SUBSCRIPTION_ID,
         base_url="https://management.usgovcloudapi.net",
         credential_scopes=['https://management.usgovcloudapi.net/.default'],
-        api_version= AZURE_DESKTOPVIRTUALIZATION_MGMT_CLIENT_API_VERSION
+        api_version=AZURE_DESKTOPVIRTUALIZATION_MGMT_CLIENT_API_VERSION
     )
 
 
-def get_avd_session_hosts(client, rg_name, host_pool_name):
-    return client.session_hosts.list(rg_name, host_pool_name)
-
-
-def list_host_pool_user_session_data():
-    api_data = {}
+def list_all_host_pools():
     avd_client = create_avd_client_session()
-    list_user_sessions = avd_client.user_sessions.list_by_host_pool(AZURE_RESOURCE_GROUP_NAME, AZURE_HOST_POOL_NAME)
-    api_data['data'] = list_user_sessions
-    if api_data['data']:
-        for _ in api_data['data']:
-            return api_data['data']
-    else:
-        print(f'nodata, {__name__}')
-        return 1
+    api_data['host_pool_data'] = []
+    host_pools = avd_client.host_pools.list()
 
-# TODO convert loop to generator
-def find_user_avd_session_data(user_upn):
-    # TODO add error handling
-    user_email = user_upn.strip()
-    # TODO better validation ?
-    if '@' in user_email:
-        for data in list_host_pool_user_session_data():
-            output_format = {"session_host_name": data.name, "upn": data.user_principal_name,
-                             "entra_id_user_name": data.active_directory_user_name}
-            if user_email == data.user_principal_name:
-                return output_format
-    else:
-        ## TODO break or exit 1 here / Logging here
-        print('no email found')
-        return 1
+    for host_pool_data in host_pools:
+        parse_rg_and_hostpool_name = host_pool_data.id.split('/')
+        resource_group = parse_rg_and_hostpool_name[4]
+        host_pool_name = parse_rg_and_hostpool_name[8]
+
+        api_data['host_pool_data'].append({
+            'resource_group': resource_group,
+            'host_pool_name': host_pool_name
+        })
+
+    return api_data['host_pool_data']
 
 
-def end_user_avd_session(user_upn):
-    # TODO add error handling
-    parse_session_host_and_user_session_id = find_user_avd_session_data(user_upn)[
-        'session_host_name'].split('/')
-    session_host_name = parse_session_host_and_user_session_id[1]
-    user_session_id = parse_session_host_and_user_session_id[2]
+def list_user_sessions(resource_group, host_pool_name):
     avd_client = create_avd_client_session()
-    return avd_client.user_sessions.delete(AZURE_RESOURCE_GROUP_NAME, AZURE_HOST_POOL_NAME, session_host_name,
-                                           user_session_id, force=True)
+    return avd_client.user_sessions.list_by_host_pool(resource_group, host_pool_name)
+
+
+def find_user_session_data(user_email):
+    upn = user_email.strip().lower()
+    user_sessions = []
+    if '@' in upn:
+        # print(user_email)
+        for host_pool_info in list_all_host_pools():
+            resource_group = host_pool_info['resource_group']
+            host_pool_name = host_pool_info['host_pool_name']
+            for user_data in list_user_sessions(resource_group, host_pool_name):
+                # print(user_data)
+                if upn == user_data.user_principal_name:
+                    user_sessions.append({'session_host_name': user_data.name,
+                                          'upn': user_data.user_principal_name,
+                                          'entra_id_user_name': user_data.active_directory_user_name,
+                                          'resource_group': resource_group,
+                                          'host_pool_name': host_pool_name
+                                          })
+    else:
+        print('Invalid email format')
+        return None
+    return user_sessions
+
+
+def end_user_session(user_email):
+    avd_client = create_avd_client_session()
+    user_sessions = find_user_session_data(user_email)
+
+    for session_data in user_sessions:
+        parse_session_host_and_user_session_id = session_data['session_host_name'].split('/')
+        session_host_name = parse_session_host_and_user_session_id[1]
+        user_session_id = parse_session_host_and_user_session_id[2]
+
+        avd_client.user_sessions.delete(
+            session_data['resource_group'],
+            session_data['host_pool_name'],
+            session_host_name,
+            user_session_id,
+            force=False
+        )
+    if not user_sessions:
+        print(f'No sessions found for: {user_email}')
+    else:
+        print(f'{len(user_sessions)} sessions ended for user: {user_email}')
 
 
 if __name__ == '__main__':
     exit(0)
-
