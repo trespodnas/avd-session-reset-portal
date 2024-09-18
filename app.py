318eb3ae138709464b27743de7dc23cca93ec98c
Jay <jnelson@LTCF99.local>
2024-09-18 19:23:48 -0400

    added user state tracking

--- app.py
+++ app.py
@@ -2,28 +2,22 @@ import  random
 import string
 
 from helpers.sleeper import end_user_session_delayed
+from helpers.user_state import unique_token, add_to_user_state, remove_user_from_state, get_user_upn_from_state, get_user_token_from_state, show_all_user_states
+
 
 from flask_executor import Executor
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 
+# TODO to env vars or config file
 APPLICATION_PORT = 5000
 APPLICATION_DEBUG = False
 APPLICATION_THREADED = True
+DUMMY_EMAIL_ADDRESS = 'enter_your_email_address@here.org'
 
-
-session_data = {
-    'dummy_upn' : 'enter_your_email_address@here.org',
-    'session_state' : {}
-}
-
-
-token_for_flask_secret_key = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
-
-                                     for _ in range(50))
 
 app = Flask(__name__)
 executor = Executor(app)
-app.secret_key = token_for_flask_secret_key
+app.secret_key = unique_token(length=50)
 
 
 @app.route('/api/upn', methods=['POST'])
@@ -31,7 +25,7 @@ def get_user_session_upn():
     upn = request.form.get('upn')
     if upn:
         session['upn'] = upn
-        session_data['session_state'][upn] = upn
+        add_to_user_state(upn)
         return jsonify({"status": "UPN received"}), 200
     return jsonify({"status": "UPN not received"}), 400
 
@@ -42,14 +36,17 @@ def index():
         email = request.form['email']
         session['email'] = email
         return redirect(url_for('get_email'))
-    return render_template('index.html', user_email=session_data['dummy_upn'])
+    return render_template('index.html', user_email=DUMMY_EMAIL_ADDRESS)
 
 
 @app.route('/get-email', methods=['GET'])
 def get_email():
     email = session.get('email')  # Retrieve email from session
-    if email in session_data['session_state']:
-        executor.submit(end_user_session_delayed, email)
+    print(show_all_user_states())
+    users_token = get_user_token_from_state(email)
+    if email in get_user_upn_from_state(email) and users_token == get_user_token_from_state(email):
+        executor.submit(end_user_session_delayed, get_user_upn_from_state(email))
+        remove_user_from_state(email)
         session.pop('email', None)  # Clear the session
         message = f"Session disconnect for: {email} successfully submitted."
         return render_template('success.html', message=message)
---
+++ helpers/user_state.py
@@ -0,0 +1,43 @@
+import random
+import string
+
+
+session_data = {'session_state': {}}
+
+
+def unique_token(length=10):
+    return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(length))
+
+
+def add_to_user_state(upn):
+    session_data['session_state'][upn] = {
+        'upn': upn,
+        'my_key': unique_token()
+    }
+
+
+def get_user_upn_from_state(email):
+    user_data = session_data['session_state'].get(email)
+    if user_data:
+        return user_data['upn']
+    else:
+        return None
+
+def get_user_token_from_state(email):
+    user_data = session_data['session_state'].get(email)
+    if user_data:
+        return user_data['my_key']
+    else:
+        return None
+
+
+def remove_user_from_state(upn):
+    del session_data['session_state'][upn]
+
+
+def show_all_user_states():
+    return session_data['session_state'].keys()
+
+
+if __name__ == '__main__':
+    exit(0)
