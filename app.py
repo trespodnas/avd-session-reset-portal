7b7648c8d00de2c71f9bac84dc9e5615643f318d
Jay <jnelson@LTCF99.local>
2024-09-19 11:30:27 -0400

    added areas for func documentation and removed other inline comments and commented out one debug print statement

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 APPLICATION_PORT = 5000
 APPLICATION_DEBUG = False
 APPLICATION_THREADED = True
-DUMMY_EMAIL_ADDRESS = 'enter_your_email_address@here.org'
+DUMMY_EMAIL_ADDRESS = 'your_email_address@here.org'
 
 
 app = Flask(__name__)
@@ -22,6 +22,10 @@ app.secret_key = unique_token(length=50)
 
 @app.route('/api/upn', methods=['POST'])
 def get_user_session_upn():
+    """
+
+    :return:
+    """
     upn = request.form.get('upn')
     if upn:
         session['upn'] = upn
@@ -32,6 +36,10 @@ def get_user_session_upn():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """
+
+    :return:
+    """
     if request.method == 'POST':
         email = request.form['email']
         session['email'] = email
@@ -41,10 +49,13 @@ def index():
 
 @app.route('/get-email', methods=['GET'])
 def get_email():
-    email = session.get('email')  # Retrieve email from session
-    print(show_all_user_states())
-    users_token = get_user_token_from_state(email)
-    if email in get_user_upn_from_state(email) and users_token == get_user_token_from_state(email):
+    """
+
+    :return:
+    """
+    email = session.get('email').lower()
+    # print(show_all_user_states())
+    if email == get_user_upn_from_state(email) and get_user_token_from_state(email):
         executor.submit(end_user_session_delayed, get_user_upn_from_state(email))
         remove_user_from_state(email)
         session.pop('email', None)  # Clear the session
