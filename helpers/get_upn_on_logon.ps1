26aecc66bfcc35e7ade1f9907a812cb50cb3b9a3
Jay <jnelson@LTCF99.local>
2024-10-17 14:40:02 -0400

    reverted upn back to original

--- helpers/get_upn_on_logon.ps1
+++ helpers/get_upn_on_logon.ps1
@@ -1,23 +1,3 @@
 # Capture user upn when user logs in and send to flask/API
-
-# Set to $false to disable logging
-$EnableLogging = $true
-
-$currentDir = Get-Location
-
 $UPN = whoami /upn
-$body = @{ upn = $UPN }
-
-try {
-    $response = Invoke-RestMethod -Uri http://localhost:5000/api/upn -Method Post -Body $body
-    $status = "Success"
-} catch {
-    $status = "Failed: $_"
-}
-
-if ($EnableLogging) {
-    $logFilePath = Join-Path -Path $currentDir -ChildPath "upn_log.txt"
-
-    $logEntry = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - UPN: $UPN, Status: $status"
-    Add-Content -Path $logFilePath -Value $logEntry
-}
+Invoke-RestMethod -Uri http://localhost:5000/api/upn -Method Post -Body @{ upn = $UPN }
